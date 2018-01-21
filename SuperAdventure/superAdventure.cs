92fb06d849129b80371e1b072bfd03c24d81bb25
ASpoonyBard3 <Samsonf21@gmail.com>
2018-01-21 14:45:25 +0000

    added labels at appro locations

--- SuperAdventure/superAdventure.cs
+++ SuperAdventure/superAdventure.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SuperAdventure
 {
-    public partial class Form1 : Form
+    public partial class superAdventure : Form
     {
-        public Form1()
+        public superAdventure()
         {
             InitializeComponent();
         }
