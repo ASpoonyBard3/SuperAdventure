c3d12073a34018322aa5cc08ef4d8d2d95399f3a
ASpoonyBard3 <Samsonf21@gmail.com>
2018-01-21 14:57:53 +0000

    Added player class and test button

--- SuperAdventure/superAdventure.cs
+++ SuperAdventure/superAdventure.cs
@@ -16,5 +16,10 @@ namespace SuperAdventure
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
     }
 }
